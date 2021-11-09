80c54558feee4c44428c579184081fa353f63223
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-11-09 08:25:12 +0300

    commit 09.11.21

--- src/main/java/ru/netology/BonusMilesService.java
+++ src/main/java/ru/netology/BonusMilesService.java
@@ -3,10 +3,8 @@ package ru.netology;
 public class BonusMilesService {
 
     int mileCost = 20; // Рублей за одну бонусную милю
-    int calculationByMile; // Миль за покупку билета
 
     public int calculate(int cost) {
-        calculationByMile = cost / mileCost;
-        return calculationByMile;
+        return cost / mileCost;
     }
 }
